b0c1c673fd7279257672b6f9cb91d3e67c6a7894
avivharuzi <avivharuzi@gmail.com>
2018-05-30 23:07:20 +0300

    added layout

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("globals/config.php");
+
+$title = "Home";
+
+?>
+
+<?php require_once("layout/header.php"); ?>
+
+<?php require_once("layout/footer.php"); ?>
---
+++ layout/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="">
+        <title><?php if (isset($title)) { echo $title; } ?></title>  
+        <link rel="stylesheet" type="text/css" href="css/styles.css">   
+        <link rel="shortcut icon" type="image/x-icon" href="">
+        <link rel="icon" type="image/png" href="">
+    </head>
+    <body>
+        <div id="app">
