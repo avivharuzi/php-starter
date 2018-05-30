e57c81dcf91a24ac99a048a8c3df041f0f14346d
avivharuzi <avivharuzi@gmail.com>
2018-05-30 23:23:36 +0300

    path fixes

--- globals/config.php
+++ globals/config.php
@@ -4,9 +4,9 @@ session_start();
 
 date_default_timezone_set("Israel");
 
-require_once("connection/db.php");
-require_once("handlers/db-handler.php");
-require_once("handlers/message-handler.php");
-require_once("handlers/validation-handler.php");
+require_once("./connection/db.php");
+require_once("./handlers/db-handler.php");
+require_once("./handlers/message-handler.php");
+require_once("./handlers/validation-handler.php");
 
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once("globals/config.php");
+require_once("./globals/config.php");
 
 $title = "Home";
 
 ?>
 
-<?php require_once("layout/header.php"); ?>
+<?php require_once("./layout/header.php"); ?>
 
-<?php require_once("layout/footer.php"); ?>
+<?php require_once("./layout/footer.php"); ?>
