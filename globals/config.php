91061e4523c7fca593f5d7a9164556a2fec28b96
avivharuzi <avivharuzi@gmail.com>
2018-05-30 23:02:00 +0300

    added globals config and empty folders

---
+++ globals/config.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+date_default_timezone_set("Israel");
+
+require_once("connection/db.php");
+require_once("handlers/db-handler.php");
+require_once("handlers/message-handler.php");
+require_once("handlers/validation-handler.php");
+
+?>
